7c97351c7ab343f606ca3c1f2cf9dc782949b3de
udaypydi <udaypydi333@gmail.com>
2020-07-11 23:41:32 +0530

    fix(add): horizontal movement of pipe

--- main.lua
+++ main.lua
@@ -6,38 +6,31 @@ function love.load()
   playingAreaHeight = 388
 
   --etc
+  local pipeSpaceYMin = 54
   pipeSpaceHeight = 100
-  pipeSpaceY = love.math.random(0, playingAreaHeight - pipeSpaceHeight)
+  pipeSpaceY = love.math.random(pipeSpaceYMin, playingAreaHeight - pipeSpaceHeight)
+  pipeX = playingAreaWidth
 end
 
 function love.update(dt)
   -- body...
   birdYSpeed = birdYSpeed + (516 * dt)
   birdY = birdY + (birdYSpeed * dt)
+  pipeX = pipeX - (60 * dt)
 end
 
 function love.keypressed(key)
   -- body...
+  love.load()
   if birdY > 0 then
     birdYSpeed = -165
   end
 end
 
--- function love.draw()
---   love.graphics.setColor(.14, .36, .46)
---   love.graphics.rectangle('fill', 0, 0, playingAreaWidth, playingAreaHeight)
---   love.graphics.setColor(.87, .84, .27)
---   love.graphics.rectangle('fill', 62, birdY, 30, 25)
---   love.graphics.setColor(.37, .82, .28)
---   love.graphics.rectangle('fill', playingAreaWidth, 0, 54, playingAreaHeight)
--- end
-
 function love.draw()
     -- etc.
 
     local pipeWidth = 54
-    local pipeSpaceY = 150
-    local pipeSpaceHeight = 100
 
     -- play area
     love.graphics.setColor(.14, .36, .46)
@@ -49,18 +42,18 @@ function love.draw()
     love.graphics.rectangle('fill', 62, birdY, 30, 25)
 
     -- pipe
-    
+
     love.graphics.setColor(.37, .82, .28)
     love.graphics.rectangle(
         'fill',
-        playingAreaWidth,
+        pipeX,
         0,
         pipeWidth,
         pipeSpaceY
     )
     love.graphics.rectangle(
         'fill',
-        playingAreaWidth,
+        pipeX,
         pipeSpaceY + pipeSpaceHeight,
         pipeWidth,
         playingAreaHeight - pipeSpaceY - pipeSpaceHeight
