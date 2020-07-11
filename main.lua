71f4a832167e7c77981279c870cfdfbb0fe48f04
udaypydi <udaypydi333@gmail.com>
2020-07-11 23:36:28 +0530

    add space in pipe

--- main.lua
+++ main.lua
@@ -4,6 +4,10 @@ function love.load()
   birdYSpeed = 0
   playingAreaWidth = 300
   playingAreaHeight = 388
+
+  --etc
+  pipeSpaceHeight = 100
+  pipeSpaceY = love.math.random(0, playingAreaHeight - pipeSpaceHeight)
 end
 
 function love.update(dt)
@@ -19,11 +23,46 @@ function love.keypressed(key)
   end
 end
 
+-- function love.draw()
+--   love.graphics.setColor(.14, .36, .46)
+--   love.graphics.rectangle('fill', 0, 0, playingAreaWidth, playingAreaHeight)
+--   love.graphics.setColor(.87, .84, .27)
+--   love.graphics.rectangle('fill', 62, birdY, 30, 25)
+--   love.graphics.setColor(.37, .82, .28)
+--   love.graphics.rectangle('fill', playingAreaWidth, 0, 54, playingAreaHeight)
+-- end
+
 function love.draw()
-  love.graphics.setColor(.14, .36, .46)
-  love.graphics.rectangle('fill', 0, 0, playingAreaWidth, playingAreaHeight)
-  love.graphics.setColor(.87, .84, .27)
-  love.graphics.rectangle('fill', 62, birdY, 30, 25)
-  love.graphics.setColor(.37, .82, .28)
-  love.graphics.rectangle('fill', playingAreaWidth, 0, 54, playingAreaHeight)
+    -- etc.
+
+    local pipeWidth = 54
+    local pipeSpaceY = 150
+    local pipeSpaceHeight = 100
+
+    -- play area
+    love.graphics.setColor(.14, .36, .46)
+    love.graphics.rectangle('fill', 0, 0, playingAreaWidth, playingAreaHeight)
+
+    -- bird
+
+    love.graphics.setColor(.87, .84, .27)
+    love.graphics.rectangle('fill', 62, birdY, 30, 25)
+
+    -- pipe
+    
+    love.graphics.setColor(.37, .82, .28)
+    love.graphics.rectangle(
+        'fill',
+        playingAreaWidth,
+        0,
+        pipeWidth,
+        pipeSpaceY
+    )
+    love.graphics.rectangle(
+        'fill',
+        playingAreaWidth,
+        pipeSpaceY + pipeSpaceHeight,
+        pipeWidth,
+        playingAreaHeight - pipeSpaceY - pipeSpaceHeight
+    )
 end
